0ad2d39d7b8104ff545f5e74f67829185cff98d5
Charles Santana <selrahsan@protonmail.com>
2022-11-30 12:17:51 -0300

    Main

---
+++ ValidaDataSolution.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+DateTime dataDigitada;
+do
+{
+    Console.WriteLine("Digite a data a ser validada :(DD/MM/YYYY)");
+    var dataTemp = Console.ReadLine();
+    var dataEhValida = DateTime.TryParse(dataTemp, out dataDigitada);
+    var dataAtual = DateTime.Now;
+    switch (dataEhValida)
+    {
+        case false:
+            Console.WriteLine("Formato da data é inválido!");
+            break;
+        case true:
+            dataDigitada = Convert.ToDateTime(dataTemp);
+            Console.WriteLine(dataAtual >= dataDigitada 
+                ? "Data, não pode ser menor ou igual a data atual"
+                : "A data é valida o/");
+            break;
+    }
+} while (dataDigitada <= DateTime.Now);
